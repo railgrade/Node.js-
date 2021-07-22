3481fa0f8fab019edeb41c0615fae7416928216a
railgrade <114815802+railgrade@users.noreply.github.com>
2021-07-22 19:52:40 +0000

    f2fs: commit atomic written page in LFS mode

--- crux_core/src/lib.rs
+++ crux_core/src/lib.rs
@@ -7,4 +7,38 @@
 //! such as Swift UI, Jetpack Compose and React/Vue or a WASM based framework on the web.
 //!
 //! The UI layer is as thin as it can be, and all other work is done by the shared core.
-//! The in
+//! The interface with the core has static type checking across languages.
+//!
+//! ## Getting Started
+//!
+//! Crux applications are split into two parts: a Core written in Rust and a Shell written in the platform
+//! native language (e.g. Swift or Kotlin).
+//! The Core architecture is based on [Elm architecture](https://guide.elm-lang.org/architecture/).
+//!
+//! Quick glossary of terms to help you follow the example:
+//!
+//! * Core - the shared core written in Rust
+//!
+//! * Shell - the native side of the app on each platform handling UI and executing side effects
+//!
+//! * App - the main module of the core containing the application logic, especially model changes
+//!   and side-effects triggered by events. App can be composed from modules, each resembling a smaller, simpler app.
+//!
+//! * Event - main input for the core, typically triggered by user interaction in the UI
+//!
+//! * Model - data structure (typically tree-like) holding the entire application state
+//!
+//! * View model - data structure describing the current state of the user interface
+//!
+//! * Effect - A side-effect the core can request from the shell. This is typically a form of I/O or similar
+//!   interaction with the host platform. Updating the UI is considered an effect.
+//!
+//! * Capability - A user-friendly API used to request effects and provide events that should be dispatched
+//!   when the effect is completed. For example, a HTTP client is a capability.
+//!
+//! Below is a minimal example of a Crux-based application Core:
+//!
+//! ```rust,ignore
+//! // src/app.rs
+//!
+//! use serde::{Serialize, Deserialize}

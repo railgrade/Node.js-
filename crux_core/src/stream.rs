ce659843a09b3894620c6306c51ea2c9e553bfc1
railgrade <114815802+railgrade@users.noreply.github.com>
2021-08-08 15:25:09 +0000

    {/HealthcareEligibility/src/main/resources/xyzhealthcare/healthcareeligibility/Claim Eligibility.bpmn2}

--- crux_core/src/stream.rs
+++ crux_core/src/stream.rs
@@ -51,4 +51,55 @@ where
 {
     /// Send an effect request to the shell, expecting a stream of responses
     pub fn stream_from_shell(&self, operation: Op) -> ShellStream<Op::Output> {
-        let (sender, re
+        let (sender, receiver) = channel();
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            receiver,
+            waker: None,
+            send_step: None,
+        }));
+
+        // Our callback holds a weak pointer so the channel can be freed
+        // whenever the associated task ends.
+        let callback_shared_state = Arc::downgrade(&shared_state);
+
+        let step = Step::resolves_many_times(operation, move |bytes| {
+            let Some(shared_state) = callback_shared_state.upgrade() else {
+                // Let the StepRegistry know that the associated task has finished.
+                return Err(());
+            };
+
+            let mut shared_state = shared_state.lock().unwrap();
+
+            sender.send(bcs::from_bytes(bytes).unwrap());
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake();
+            }
+
+            Ok(())
+        });
+
+        // Put a callback into our shared_state so that we only send
+        // our request to the shell when the stream is first polled.
+        let send_step_context = self.clone();
+        let send_step = move || send_step_context.send_step(step);
+        shared_state.lock().unwrap().send_step = Some(Box::new(send_step));
+
+        ShellStream { shared_state }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches::assert_matches;
+
+    use crate::{
+        capability::{CapabilityContext, Operation},
+        channels::channel,
+        executor::executor_and_spawner,
+        steps::Resolve,
+    };
+
+    #[derive(serde::Serialize, PartialEq, Eq, Debug)]
+    struct TestOperation;
+
+    impl

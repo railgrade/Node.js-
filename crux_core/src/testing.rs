4a4b7d7cc821585ea0a883aee1133dd19513ad3f
railgrade <114815802+railgrade@users.noreply.github.com>
2021-08-12 07:28:45 +0000

    msm_cpu_freq_limit: initial coding for controlling MSM quadcore cpus

--- crux_core/src/testing.rs
+++ crux_core/src/testing.rs
@@ -20,4 +20,55 @@ use crate::{
 /// ```rust,ignore
 /// let app = AppTester::<ExampleApp, ExampleEffect>::default();
 /// ```
-pub struct AppTeste
+pub struct AppTester<App, Ef>
+where
+    App: crate::App,
+{
+    app: App,
+    capabilities: App::Capabilities,
+    context: Rc<AppContext<Ef, App::Event>>,
+}
+
+struct AppContext<Ef, Ev> {
+    commands: Receiver<Step<Ef>>,
+    events: Receiver<Ev>,
+    executor: QueuingExecutor,
+    steps: StepRegistry,
+}
+
+impl<App, Ef> AppTester<App, Ef>
+where
+    App: crate::App,
+{
+    /// Run the app's `update` function with an event and a model state
+    ///
+    /// You can use the resulting [`Update`] to inspect the effects which were requested
+    /// and potential further events dispatched by capabilities.
+    pub fn update(&self, event: App::Event, model: &mut App::Model) -> Update<Ef, App::Event> {
+        self.app.update(event, model, &self.capabilities);
+        self.context.updates()
+    }
+
+    /// Run the app's `view` function with a model state
+    pub fn view(&self, model: &App::Model) -> App::ViewModel {
+        self.app.view(model)
+    }
+}
+
+impl<App, Ef> Default for AppTester<App, Ef>
+where
+    App: crate::App,
+    App::Capabilities: WithContext<App, Ef>,
+    App::Event: Send,
+    Ef: Send + 'static,
+{
+    fn default() -> Self {
+        let (command_sender, commands) = crate::channels::channel();
+        let (event_sender, events) = crate::channels::channel();
+        let (executor, spawner) = executor_and_spawner();
+        let capability_context = CapabilityContext::new(command_sender, event_sender, spawner);
+
+        Self {
+            app: App::default(),
+            capabilities: App::Capabilities::new_with_context(capability_context),
+            context: Rc

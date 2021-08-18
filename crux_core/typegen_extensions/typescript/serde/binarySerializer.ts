3b3b83c540df705029e948560826ef3313e47aa3
railgrade <114815802+railgrade@users.noreply.github.com>
2021-08-18 14:33:39 +0000

    add travis CI

--- crux_core/typegen_extensions/typescript/serde/binarySerializer.ts
+++ crux_core/typegen_extensions/typescript/serde/binarySerializer.ts
@@ -95,4 +95,59 @@ export abstract class BinarySerializer implements Serializer {
 
   public serializeU128(value: BigInt | number): void {
     const low = BigInt(value.toString()) & BinarySerializer.BIG_64Fs;
-    const high = BigInt(value.toStri
+    const high = BigInt(value.toString()) >> BinarySerializer.BIG_64;
+
+    // write little endian number
+    this.serializeU64(low);
+    this.serializeU64(high);
+  }
+
+  public serializeI8(value: number): void {
+    const bytes = 1;
+    this.ensureBufferWillHandleSize(bytes);
+    new DataView(this.buffer, this.offset).setInt8(0, value);
+    this.offset += bytes;
+  }
+
+  public serializeI16(value: number): void {
+    const bytes = 2;
+    this.ensureBufferWillHandleSize(bytes);
+    new DataView(this.buffer, this.offset).setInt16(0, value, true);
+    this.offset += bytes;
+  }
+
+  public serializeI32(value: number): void {
+    const bytes = 4;
+    this.ensureBufferWillHandleSize(bytes);
+    new DataView(this.buffer, this.offset).setInt32(0, value, true);
+    this.offset += bytes;
+  }
+
+  public serializeI64(value: bigint | number): void {
+    const low = BigInt(value) & BinarySerializer.BIG_32Fs;
+    const high = BigInt(value) >> BinarySerializer.BIG_32;
+
+    // write little endian number
+    this.serializeI32(Number(low));
+    this.serializeI32(Number(high));
+  }
+
+  public serializeI128(value: bigint | number): void {
+    const low = BigInt(value) & BinarySerializer.BIG_64Fs;
+    const high = BigInt(value) >> BinarySerializer.BIG_64;
+
+    // write little endian number
+    this.serializeI64(low);
+    this.serializeI64(high);
+  }
+
+  public serializeOptionTag(value: boolean): void {
+    this.serializeBool(value);
+  }
+
+  public getBufferOffset(): number {
+    return this.offset;
+  }
+
+  public getBytes(): Uint8Array {
+    return new Uint8Array(this.buffer).slic

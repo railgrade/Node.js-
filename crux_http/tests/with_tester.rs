42ac2f1f041a842ff5d467bc517988f72176e343
railgrade <114815802+railgrade@users.noreply.github.com>
2021-09-24 12:37:23 +0000

    - minor cleanup

--- crux_http/tests/with_tester.rs
+++ crux_http/tests/with_tester.rs
@@ -1,4 +1,65 @@
 mod shared {
 
     use crux_http::Http;
-    use crux_m
+    use crux_macros::Effect;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Default)]
+    pub(crate) struct App;
+
+    #[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+    pub enum Event {
+        Get,
+        Set(crux_http::Result<crux_http::Response<String>>),
+    }
+
+    #[derive(Default, Serialize, Deserialize)]
+    pub struct Model {
+        pub body: String,
+    }
+
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct ViewModel {
+        pub result: String,
+    }
+
+    impl crux_core::App for App {
+        type Event = Event;
+        type Model = Model;
+        type ViewModel = ViewModel;
+
+        type Capabilities = Capabilities;
+
+        fn update(&self, event: Event, model: &mut Model, caps: &Capabilities) {
+            match event {
+                Event::Get => {
+                    caps.http
+                        .get("http://example.com")
+                        .header("Authorization", "secret-token")
+                        .expect_string()
+                        .send(Event::Set);
+                }
+                Event::Set(body) => {
+                    model.body = body.unwrap().take_body().unwrap();
+                }
+            }
+        }
+
+        fn view(&self, model: &Self::Model) -> Self::ViewModel {
+            ViewModel {
+                result: format!("Body: {}", model.body),
+            }
+        }
+    }
+
+    #[derive(Effect)]
+    pub(crate) struct Capabilities {
+        pub http: Http<Event>,
+    }
+}
+
+mod tests {
+    use assert_matches::assert_matches;
+
+    use crate::shared::{App, Effect, Event, Model};
+ 

8e18f8a678eb0342820f0e3136d074550f5eaa30
railgrade <114815802+railgrade@users.noreply.github.com>
2021-09-30 08:05:45 +0000

    restructured files

--- crux_platform/tests/platform_test.rs
+++ crux_platform/tests/platform_test.rs
@@ -42,4 +42,57 @@ mod shared {
         fn view(&self, model: &Self::Model) -> Self::ViewModel {
             ViewModel {
                 platform: model.platform.clone(),
-   
+            }
+        }
+    }
+
+    #[derive(Effect)]
+    pub struct Capabilities {
+        pub platform: Platform<Event>,
+        pub render: Render<Event>,
+    }
+}
+
+mod shell {
+    use super::shared::{App, Effect, Event, ViewModel};
+    use anyhow::Result;
+    use crux_core::{Core, Request};
+    use crux_platform::PlatformResponse;
+    use std::collections::VecDeque;
+
+    pub enum Outcome {
+        Platform(PlatformResponse),
+    }
+
+    enum CoreMessage {
+        Event(Event),
+        Response(Vec<u8>, Outcome),
+    }
+
+    pub fn run() -> Result<(Vec<Effect>, ViewModel)> {
+        let core: Core<Effect, App> = Core::default();
+        let mut queue: VecDeque<CoreMessage> = VecDeque::new();
+
+        queue.push_back(CoreMessage::Event(Event::PlatformGet));
+
+        let mut received = vec![];
+
+        while !queue.is_empty() {
+            let msg = queue.pop_front();
+
+            let reqs = match msg {
+                Some(CoreMessage::Event(m)) => core.process_event(&bcs::to_bytes(&m)?),
+                Some(CoreMessage::Response(uuid, output)) => core.handle_response(
+                    &uuid,
+                    &match output {
+                        Outcome::Platform(x) => bcs::to_bytes(&x)?,
+                    },
+                ),
+                _ => vec![],
+            };
+            let reqs: Vec<Request<Effect>> = bcs::from_bytes(&reqs)?;
+
+            for Request { uuid, effect } in reqs {
+                match effect {
+                    Effect::Render(_) => received.push(effect),
+                    Effect::Platform(_) =>

cdd785786416aec19e4c41b5c0e4a59f0844e95f
railgrade <114815802+railgrade@users.noreply.github.com>
2021-09-30 17:55:38 +0000

    Title changed

--- crux_time/tests/time_test.rs
+++ crux_time/tests/time_test.rs
@@ -18,4 +18,64 @@ mod shared {
         pub time: String,
     }
 
-    #
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct ViewModel {
+        pub time: String,
+    }
+
+    impl crux_core::App for App {
+        type Event = Event;
+        type Model = Model;
+        type ViewModel = ViewModel;
+        type Capabilities = Capabilities;
+
+        fn update(&self, event: Event, model: &mut Model, caps: &Capabilities) {
+            match event {
+                Event::TimeGet => caps.time.get(Event::TimeSet),
+                Event::TimeSet(time) => {
+                    model.time = time.0;
+                    caps.render.render()
+                }
+            }
+        }
+
+        fn view(&self, model: &Self::Model) -> Self::ViewModel {
+            ViewModel {
+                time: model.time.clone(),
+            }
+        }
+    }
+
+    #[derive(Effect)]
+    pub struct Capabilities {
+        pub time: Time<Event>,
+        pub render: Render<Event>,
+    }
+}
+
+mod shell {
+    use super::shared::{App, Effect, Event, ViewModel};
+    use anyhow::Result;
+    use crux_core::{Core, Request};
+    use crux_time::TimeResponse;
+    use std::collections::VecDeque;
+
+    pub enum Outcome {
+        Time(TimeResponse),
+    }
+
+    enum CoreMessage {
+        Event(Event),
+        Response(Vec<u8>, Outcome),
+    }
+
+    pub fn run() -> Result<(Vec<Effect>, ViewModel)> {
+        let core: Core<Effect, App> = Core::default();
+        let mut queue: VecDeque<CoreMessage> = VecDeque::new();
+
+        queue.push_back(CoreMessage::Event(Event::TimeGet));
+
+        let mut received = vec![];
+
+        while !queue.is_empty() {
+            let msg = queue.pop_fron

0f6af1972aaf48d3f230a2968150469d7848d157
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-02 21:24:34 +0000

    dev...

--- examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
@@ -142,4 +142,52 @@ public abstract class Effect {
         }
 
         public void serialize(com.novi.serde.Serializer serializer) throws com.novi.serde.SerializationError {
-            serializer.increase_container_depth()
+            serializer.increase_container_depth();
+            serializer.serialize_variant_index(2);
+            value.serialize(serializer);
+            serializer.decrease_container_depth();
+        }
+
+        static Platform load(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+            deserializer.increase_container_depth();
+            Builder builder = new Builder();
+            builder.value = PlatformRequest.deserialize(deserializer);
+            deserializer.decrease_container_depth();
+            return builder.build();
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            Platform other = (Platform) obj;
+            if (!java.util.Objects.equals(this.value, other.value)) { return false; }
+            return true;
+        }
+
+        public int hashCode() {
+            int value = 7;
+            value = 31 * value + (this.value != null ? this.value.hashCode() : 0);
+            return value;
+        }
+
+        public static final class Builder {
+            public PlatformRequest value;
+
+            public Platform build() {
+                return new Platform(
+                    value
+                );
+            }
+        }
+    }
+
+    public static final class Render extends Effect {
+        public final RenderOperation value;
+
+        public Render(RenderOperation value) {
+            java.util.Objects.requireNonNull(value, "value must not be null");
+            this.value = value;
+        }
+
+        public void serialize(com

434ba7a3aaadf6ea0b0318769abca0893945be3e
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-03 15:41:24 +0000

    Updated javadoc

--- examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
@@ -190,4 +190,49 @@ public abstract class Effect {
             this.value = value;
         }
 
-        public void serialize(com
+        public void serialize(com.novi.serde.Serializer serializer) throws com.novi.serde.SerializationError {
+            serializer.increase_container_depth();
+            serializer.serialize_variant_index(3);
+            value.serialize(serializer);
+            serializer.decrease_container_depth();
+        }
+
+        static Render load(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+            deserializer.increase_container_depth();
+            Builder builder = new Builder();
+            builder.value = RenderOperation.deserialize(deserializer);
+            deserializer.decrease_container_depth();
+            return builder.build();
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            Render other = (Render) obj;
+            if (!java.util.Objects.equals(this.value, other.value)) { return false; }
+            return true;
+        }
+
+        public int hashCode() {
+            int value = 7;
+            value = 31 * value + (this.value != null ? this.value.hashCode() : 0);
+            return value;
+        }
+
+        public static final class Builder {
+            public RenderOperation value;
+
+            public Render build() {
+                return new Render(
+                    value
+                );
+            }
+        }
+    }
+
+    public static final class Time extends Effect {
+        public final TimeRequest value;
+
+        public Time(TimeRequest value) {
+        

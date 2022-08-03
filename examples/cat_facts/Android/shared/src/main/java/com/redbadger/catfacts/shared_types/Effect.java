a90dc4c9335a27a92c996e4e905357ac2c919279
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-03 18:09:12 +0000

    Update config.json

--- examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/Effect.java
@@ -235,4 +235,44 @@ public abstract class Effect {
         public final TimeRequest value;
 
         public Time(TimeRequest value) {
-        
+            java.util.Objects.requireNonNull(value, "value must not be null");
+            this.value = value;
+        }
+
+        public void serialize(com.novi.serde.Serializer serializer) throws com.novi.serde.SerializationError {
+            serializer.increase_container_depth();
+            serializer.serialize_variant_index(4);
+            value.serialize(serializer);
+            serializer.decrease_container_depth();
+        }
+
+        static Time load(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+            deserializer.increase_container_depth();
+            Builder builder = new Builder();
+            builder.value = TimeRequest.deserialize(deserializer);
+            deserializer.decrease_container_depth();
+            return builder.build();
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            Time other = (Time) obj;
+            if (!java.util.Objects.equals(this.value, other.value)) { return false; }
+            return true;
+        }
+
+        public int hashCode() {
+            int value = 7;
+            value = 31 * value + (this.value != null ? this.value.hashCode() : 0);
+            return value;
+        }
+
+        public static final class Builder {
+            public TimeRequest value;
+
+            public Time build() {
+                return new Time(
+                    value
+  

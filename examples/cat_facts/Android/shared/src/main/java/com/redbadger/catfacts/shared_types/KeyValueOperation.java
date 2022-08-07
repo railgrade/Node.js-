39b0a6aeba72373642e4bb06246198a19009eefc
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-07 10:33:56 +0000

    changes ...

--- examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/KeyValueOperation.java
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/KeyValueOperation.java
@@ -22,4 +22,40 @@ public abstract class KeyValueOperation {
 
     public static KeyValueOperation bcsDeserialize(byte[] input) throws com.novi.serde.DeserializationError {
         if (input == null) {
-            
+             throw new com.novi.serde.DeserializationError("Cannot deserialize null array");
+        }
+        com.novi.serde.Deserializer deserializer = new com.novi.bcs.BcsDeserializer(input);
+        KeyValueOperation value = deserialize(deserializer);
+        if (deserializer.get_buffer_offset() < input.length) {
+             throw new com.novi.serde.DeserializationError("Some input bytes were not read");
+        }
+        return value;
+    }
+
+    public static final class Read extends KeyValueOperation {
+        public final String value;
+
+        public Read(String value) {
+            java.util.Objects.requireNonNull(value, "value must not be null");
+            this.value = value;
+        }
+
+        public void serialize(com.novi.serde.Serializer serializer) throws com.novi.serde.SerializationError {
+            serializer.increase_container_depth();
+            serializer.serialize_variant_index(0);
+            serializer.serialize_str(value);
+            serializer.decrease_container_depth();
+        }
+
+        static Read load(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+            deserializer.increase_container_depth();
+            Builder builder = new Builder();
+            builder.value = deserializer.deserialize_str();
+            deserializer.decrease_container_depth();
+            return builder.build();
+        }
+
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return fals

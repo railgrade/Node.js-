b4118535aee31b0aa66175d70ff701aa752e44f7
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-10 08:30:13 +0000

    Fix comment

--- examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/RenderOperation.java
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/RenderOperation.java
@@ -16,4 +16,42 @@ public final class RenderOperation {
         return serializer.get_bytes();
     }
 
-    public static RenderOperation deserialize(com.novi.serde.Deserializer deserializer) throws com.n
+    public static RenderOperation deserialize(com.novi.serde.Deserializer deserializer) throws com.novi.serde.DeserializationError {
+        deserializer.increase_container_depth();
+        Builder builder = new Builder();
+        deserializer.decrease_container_depth();
+        return builder.build();
+    }
+
+    public static RenderOperation bcsDeserialize(byte[] input) throws com.novi.serde.DeserializationError {
+        if (input == null) {
+             throw new com.novi.serde.DeserializationError("Cannot deserialize null array");
+        }
+        com.novi.serde.Deserializer deserializer = new com.novi.bcs.BcsDeserializer(input);
+        RenderOperation value = deserialize(deserializer);
+        if (deserializer.get_buffer_offset() < input.length) {
+             throw new com.novi.serde.DeserializationError("Some input bytes were not read");
+        }
+        return value;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        RenderOperation other = (RenderOperation) obj;
+        return true;
+    }
+
+    public int hashCode() {
+        int value = 7;
+        return value;
+    }
+
+    public static final class Builder {
+        public RenderOperation build() {
+            return new RenderOperation(
+            );
+        }
+    }
+}
---
+++ examples/cat_facts/Android/shared/src/main/java/com/redbadger/catfacts/shared_types/TimeRequest.java
@@ -0,0 +1,8 @@
+package com.redbadger.catfacts.shared_types;
+
+
+public final class TimeRequest {
+    public TimeRequest() {
+    }
+
+    public void serialize(com.novi.serde.Serializer se

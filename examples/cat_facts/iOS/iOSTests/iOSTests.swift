7f3c5d0371d5fdd9bbb3ed010dd07079e591f236
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-23 12:34:28 +0000

    Added build status image

---
+++ examples/cat_facts/iOS/iOSTests/iOSTests.swift
@@ -0,0 +1,25 @@
+//
+//  iOSTests.swift
+//  iOSTests
+//
+//  Created by Stuart Harris on 08/10/2022.
+//
+
+import XCTest
+@testable import iOS
+
+final class iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your

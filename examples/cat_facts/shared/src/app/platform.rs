831d459041d231ee19a07396f55189e579eb967a
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-23 20:11:31 +0000

    0.4.14

---
+++ examples/cat_facts/shared/src/app/platform.rs
@@ -0,0 +1,33 @@
+use crux_core::{render::Render, App};
+use crux_platform::{Platform as PlatformCap, PlatformResponse};
+use serde::{Deserialize, Serialize};
+
+#[derive(Default)]
+pub struct Platform {}
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct Model {
+    pub platform: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub enum PlatformEvent {
+    Get,
+    Set(PlatformResponse),
+}
+
+pub struct PlatformCapabilities {
+    pub platform: PlatformCap<PlatformEvent>,
+    pub render: Render<PlatformEvent>,
+}
+
+impl App for Platform {
+    type Event = PlatformEvent;
+    type Model = Model;
+    type ViewModel = Model;
+    type Capabilities = PlatformCapabilities;
+
+    fn update(&self, msg: PlatformEvent, model: &mut Model, caps: &PlatformCapabilities) {
+        match msg {
+            PlatformEvent::Get => caps.platform.get(PlatformEvent::Set),
+   

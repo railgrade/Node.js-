cd1aebc176513899aea9fb01666327498f2ca8f4
railgrade <114815802+railgrade@users.noreply.github.com>
2022-08-27 06:29:57 +0000

    dump version

--- examples/cat_facts/web-nextjs/next.config.js
+++ examples/cat_facts/web-nextjs/next.config.js
@@ -7,4 +7,48 @@ const nextConfig = {
   swcMinify: true,
 };
 
-// see https://github.com/wasm-tool/wasm-pack
+// see https://github.com/wasm-tool/wasm-pack-plugin/issues/112
+let loaded = false;
+
+module.exports = {
+  webpack(nextConfig) {
+    if (!loaded) {
+      nextConfig.plugins.push(
+        new WasmPackPlugin({
+          crateDirectory: path.resolve(__dirname, "..", "shared"),
+          extraArgs: "--target web",
+          outDir: path.resolve(__dirname, "shared", "core"),
+        })
+      );
+      loaded = true;
+    }
+
+    return nextConfig;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' 'unsafe-inline' 'unsafe-eval' catfact.ninja *.tenor.com aws.random.cat *.dream.io cdn.jsdelivr.net",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "https://aws.random.cat/meow",
+      },
+    ];
+  },
+};

c62d3e0498967974d72dba9a4ade70289251e4f3
railgrade <114815802+railgrade@users.noreply.github.com>
2022-10-12 10:18:13 +0000

    ignored

--- examples/counter/Android/shared/src/test/java/com/example/shared/ExampleUnitTest.kt
+++ examples/counter/Android/shared/src/test/java/com/example/shared/ExampleUnitTest.kt
@@ -7,4 +7,11 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [te
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

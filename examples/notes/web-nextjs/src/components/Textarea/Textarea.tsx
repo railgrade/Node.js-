9ed082c71546399d6d679a837706beea4d5ac3d1
railgrade <114815802+railgrade@users.noreply.github.com>
2023-03-01 20:23:35 +0000

    add notice

--- examples/notes/web-nextjs/src/components/Textarea/Textarea.tsx
+++ examples/notes/web-nextjs/src/components/Textarea/Textarea.tsx
@@ -44,3 +44,56 @@ const Textarea: FC<TextareaProps> = ({
   selectionEnd,
   onSelect,
   onChange,
+  className,
+}) => {
+  const taRef = useRef(null);
+  useEffect(() => {
+    if (taRef.current == null) return;
+    let ta: HTMLInputElement = taRef.current;
+
+    // Convert selection start and end to byte offsets
+
+    ta.selectionStart = ucToBytes(value, selectionStart);
+    ta.selectionEnd = ucToBytes(value, selectionEnd);
+
+    ta.addEventListener("beforeinput", onBeforeInput);
+    ta.addEventListener("input", onInput);
+
+    return () => {
+      ta.removeEventListener("beforeinput", onBeforeInput);
+      ta.removeEventListener("input", onInput);
+    };
+  });
+
+  const beforeEdit = useRef<EditState>({
+    selectionEnd: 0,
+    length: 0,
+    text: "",
+  });
+
+  const onBeforeInput = (event: InputEvent) => {
+    if (event.target == null) return;
+    let target = event.target as HTMLInputElement;
+
+    // This may become quite painful.
+    let text = event.data ?? (event.inputType == "insertLineBreak" ? "\n" : "");
+
+    beforeEdit.current.selectionEnd = target.selectionEnd ?? 0;
+    beforeEdit.current.length = target.value.length;
+    beforeEdit.current.text = text;
+  };
+
+  const onInput = (event: Event): any => {
+    if (event.target == null) return;
+    let target = event.target as HTMLInputElement;
+
+    let length = target.value.length;
+    let end = beforeEdit.current.selectionEnd;
+    let text = beforeEdit.current.text;
+
+    let removed = text.length - length + beforeEdit.current.length;
+    let start = end - removed;
+
+    onChange({
+      start: bytesToUc(value, start),
+      end: bytesToUc(value, e
